9104079e308fb241317761662def583834445121
Matheus <math.ss.oliveira@gmail.com>
2023-03-14 20:07:26 +0100

    Add pdf preview

--- edocmate/documents/admin.py
+++ edocmate/documents/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Document
 
-from .models import Document, Tag
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file_preview', 'date_created', 'date_modified')
 
-admin.site.register(Document)
-admin.site.register(Tag)
+    def file_preview(self, obj):
+        if obj.file:
+            if obj.file.name.endswith('.pdf'):
+                return format_html('<embed src="{}#view=FitH" width="300" height="200" type="application/pdf"/>',
+                                   obj.file.url)
+            elif obj.file.name.endswith('.jpg') or obj.file.name.endswith('.jpeg') or obj.file.name.endswith('.png'):
+                return format_html('<img src="{}" width="300" height="200" />', obj.file.url)
+        return '-'
+
+    file_preview.short_description = 'File Preview'
+
+
+admin.site.register(Document, DocumentAdmin)

eb5e75553a68802de84c79e14abea32cbf1a5922
Matheus <math.ss.oliveira@gmail.com>
2023-03-18 02:15:18 +0100

    Accept all formats and transform all to pdf

--- edocmate/documents/admin.py
+++ edocmate/documents/admin.py
@@ -5,6 +5,8 @@ from pdf2image import convert_from_bytes
 import io
 import pytesseract
 import PyPDF2
+from PIL import Image
+
 
 class LabelInline(admin.TabularInline):
     model = Document.labels.through
@@ -30,22 +32,25 @@ class DocumentAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         # process the pdf file only if it's a new upload
-        if not change:
-            # get the uploaded file
-            uploaded_file = form.cleaned_data['file']
+        # get the uploaded file
+        uploaded_file = form.cleaned_data['file']
 
-            # convert the pdf file to searchable pdf
+        if uploaded_file.name.endswith('.pdf'):
             images = convert_from_bytes(uploaded_file.read())
-            pdf_writer = PyPDF2.PdfWriter()
-            for image in images:
-                page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
-                pdf_reader = PyPDF2.PdfReader(io.BytesIO(page))
-                pdf_writer.add_page(pdf_reader.pages[0])
-            # save the searchable pdf to the pdf_file field
-            output_file = io.BytesIO()
-            pdf_writer.write(output_file)
+        else:
+            images = [Image.open(uploaded_file)]
 
-            obj.file.save(uploaded_file.name, output_file)
+        # convert the pdf file to searchable pdf
+        pdf_writer = PyPDF2.PdfWriter()
+        for image in images:
+            page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
+            pdf_reader = PyPDF2.PdfReader(io.BytesIO(page))
+            pdf_writer.add_page(pdf_reader.pages[0])
+        # save the searchable pdf to the pdf_file field
+        output_file = io.BytesIO()
+        pdf_writer.write(output_file)
+
+        obj.file.save('.'.join(uploaded_file.name.split('.')[:-1]) + '.pdf', output_file)
         super().save_model(request, obj, form, change)
 
 class DocumentLabelAdmin(admin.ModelAdmin):
